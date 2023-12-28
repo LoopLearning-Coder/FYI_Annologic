498ba8d0c55525538aa9286586aeb902cc803ce0
LoopLearning-Coder <153282769+LoopLearning-Coder@users.noreply.github.com>
2023-12-28 14:08:30 +0000

    Update app.py

--- app.py
+++ app.py
@@ -186,7 +186,9 @@ def main():
             new_document = string_data
             plagiarism_results = check_plagiarism(new_document, training_docs)
             st.write(f'The input lyric is most similar to the lyrics in {plagiarism_results[0][0]} with a similarity score of {plagiarism_results[0][1]}')
-            if st.button(f"Inspect {plagiarism_results[0][0]}"):
+            
+            viewsong = st.checkbox(f'Inspect {plagiarism_results[0][0]}')
+            if viewsong:
                 with open(f'./{plagiarism_results[0][0]}', 'r', encoding='utf-8') as file:
                     content = file.read()
                 st.write(content)
