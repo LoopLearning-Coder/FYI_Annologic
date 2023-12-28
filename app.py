b9ff42e5a1c294db566b4c36414e30f2b1ea3ce2
LoopLearning-Coder <153282769+LoopLearning-Coder@users.noreply.github.com>
2023-12-28 13:18:31 +0000

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,9 @@ import xgboost
 
 st.set_page_config(layout='wide', initial_sidebar_state='expanded')
 
+with open("style.css") as f:
+    st.markdown(f'<style>{f.read()}</style>',unsafe_allow_html=True)
+
 st.title('FYI - Annologic Demo')
 
 def main():
